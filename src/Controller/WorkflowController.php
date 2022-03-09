c1589d500661ffa30be88addf1e681cc202af669
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-03-09 17:53:57 +0100

    feat(WorkflowController): update

--- src/Controller/WorkflowController.php
+++ src/Controller/WorkflowController.php
@@ -21,15 +21,45 @@ class WorkflowController extends AbstractController
     #[Route('/workflow', name: 'app_workflow')]
     public function index(): Response
     {
-        $pr = new PullRequest();
-        $prWorkflow = $this->workflows->get($pr, 'pull_request');
+//        $pr = new PullRequest();
+//        $prWorkflow = $this->workflows->get($pr, 'pull_request');
+//        dump($prWorkflow);
 
-        dd($prWorkflow);
+        $post = new BlogPost();
+        $postWorkflow = $this->workflows->get($post, 'blog_publishing');
+//        dump($postWorkflow);
 
-        $bp = new BlogPost();
-        $bpWorkflow = $this->workflows->get($bp, 'blog_publishing');
+        dump(
+            '1. publish',
+            $postWorkflow->can($post, 'publish')
+        ); // False
 
-        dd($bpWorkflow);
+        dump(
+            '1. to_review',
+            $postWorkflow->can($post, 'to_review')
+        ); // True
+
+//        dump(
+//            '1. enabled transitions',
+//            $postWorkflow->getEnabledTransitions($post)
+//        );
+
+//        // Update the currentState on the post
+//        try {
+//            $postWorkflow->apply($post, 'to_review');
+//        } catch (LogicException $exception) {
+//            // ...
+//        }
+
+//        dump(
+//            '2. publish',
+//            $postWorkflow->can($post, 'publish')
+//        ); // False
+
+//        dump(
+//            '2. to_review',
+//            $postWorkflow->can($post, 'to_review')
+//        ); // True
 
         return $this->json([
             'message' => 'Welcome to your new controller!',
